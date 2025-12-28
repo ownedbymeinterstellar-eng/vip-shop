697e318c3147bc338e4b28fc52212342535cc57a
Harakiri <harakiriron@gmail.com>
2025-12-28 13:16:10 +0100

    Revert to original working reCAPTCHA keys

--- vip-shop-frontend/script.js
+++ vip-shop-frontend/script.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
     (window.location.hostname === 'localhost') ? 'http://localhost:3000' : 'https://api.vipshop.cloud';
 
-const RECAPTCHA_SITE_KEY = '6LclTjksAAAAAAgjChVmU-e9nTLYVFIKQn_LgMBP';
+const RECAPTCHA_SITE_KEY = '6LcspTgsAAAAALKJxXU1QPKKYqGxi_KHObsxVlvO';
 
 const appState = {
     currentProduct: null,
